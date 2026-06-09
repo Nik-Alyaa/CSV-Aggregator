1c39c40eec394a183faaecfdd362c008d0b3c2a0
nikalyaa <nikalyaa@tdg.local>
2026-06-09 09:51:17 +0800

    try vercel

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/CSV-Aggregator",
-  assetPrefix: "/CSV-Aggregator",
 };
 
 export default nextConfig;
