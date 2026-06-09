0b8935f80ce5359cb730c8024d0ccbcb5862d7a9
nikalyaa <nikalyaa@tdg.local>
2026-06-09 09:09:24 +0800

    git page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/CSV-Aggregator",
+  assetPrefix: "/CSV-Aggregator",
 };
 
 export default nextConfig;
